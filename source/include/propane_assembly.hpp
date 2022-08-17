7882362528809fa7bb25b0df484555e389d43f48
AggroBird <jan@aggrobird.com>
2022-08-17 17:19:38 +0900

    Added comment to assembly data

--- source/include/propane_assembly.hpp
+++ source/include/propane_assembly.hpp
@@ -25,10 +25,13 @@ namespace propane
         toolchain_version version() const noexcept;
         bool is_compatible() const noexcept;
 
+        // Direct reference to assembly data
         const assembly_data& assembly_ref() const noexcept;
+        // Byte data of assembly (minus validation header/footer)
         std::span<const uint8_t> assembly_binary() const noexcept;
-
+        // All data (including validation header/footer)
         std::span<const uint8_t> data() const noexcept;
+
         bool load(std::span<const uint8_t> from_bytes);
 
     private:
